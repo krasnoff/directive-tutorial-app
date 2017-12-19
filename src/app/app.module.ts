998952aa45c8e1cae5a050e483da76fc4bc7e1ac
krasnoff <krasnoff.kobi@gmail.com>
2017-12-19 22:14:55 +0200

    start structual directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { InputNumbersOnlyDirective } from './input-numbers-only.directive';
+import { ProgressBarDirective } from './progress-bar.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponentComponent,
-    InputNumbersOnlyDirective
+    InputNumbersOnlyDirective,
+    ProgressBarDirective
   ],
   imports: [
     BrowserModule,
